fac65092093be343f542daf62071f91356dea168
Anurag <33426386+thejoker542@users.noreply.github.com>
2025-01-12 17:57:32 +0530

    Create ChartButton.svelte

---
+++ y/src/ChartButton.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Button } from 'shadcn-svelte';
+  import { indexStore, expiryDateStore, strikePriceStore } from '../stores';
+  import { fetchChartData } from '../api';
+
+  function handleClick() {
+    const index = $indexStore;
+    const expiryDate = $expiryDateStore;
+    const strikePrice = $strikePriceStore;
+    // Fetch chart data and update the chart
+    fetchChartData(index, expiryDate, strikePrice).then(data => {
+      // Update chart with data
+    });
+  }
+</script>
+
+<Button on:click={handleClick}>Load Chart</Button>
