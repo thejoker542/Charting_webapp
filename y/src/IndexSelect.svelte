ed0e58b06b4d92bf81f20ec9e9fd1f4d3b614030
Anurag <33426386+thejoker542@users.noreply.github.com>
2025-01-12 17:56:16 +0530

    Create IndexSelect.svelte

---
+++ y/src/IndexSelect.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from 'shadcn-svelte';
+  import { indexStore } from '../stores';
+
+  let indexes = ['NIFTY', 'BANKNIFTY', 'MIDCAPNIFTY', 'SENSEX', 'BANKEX'];
+  let selectedIndex = $indexStore;
+
+  function handleSelect(event) {
+    selectedIndex = event.target.value;
+    indexStore.set(selectedIndex);
+  }
+</script>
+
+<Select on:change={handleSelect} value={selectedIndex}>
+  <SelectTrigger>
+    <SelectValue placeholder="Select Index" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectGroup>
+      {#each indexes as index}
+        <SelectItem value={index}>{index}</SelectItem>
+      {/each}
+    </SelectGroup>
+  </SelectContent>
+</Select>
